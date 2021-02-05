c2d25daf5cc32235cd1244e0c7226e2a5acf6657
bilpeixoto <bil.peixoto@gmail.com>
2021-02-05 17:54:46 -0300

    Código após a aula 4

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -5,7 +5,8 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        Atendimento.adiciona(req.body)
-        res.send('POST atendimentos')   
+
+        Atendimento.adiciona(req.body, res)
+        
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,15 +1,43 @@
 const connection = require('../controllers/infraestrutura/conection')
+const moment = require('moment')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
 
-        connection.query(sql, atendimento, (erro, resultados) => {
-            if(erro) console.log(erro)
-            else {
-                console.log(resultados)
+        // const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        // const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: moment(data).isSameOrAfter(dataCriacao),
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: atendimento.cliente.length >= 5,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+
+
+        if(erros.length) res.status(400).json(erros)
+        else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+    
+            connection.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) res.status(400).json(erro)
+                else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
     }
 }
 
